3dbd98595d1c94fda1fe1ccd723faf6c49a4e96a
joseangelsamperevazquez <jsamperevazquez@danielcastelao.org>
2022-01-07 12:57:50 +0100

    Cambio imagen a función para hacer mas modular la app

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,22 +32,28 @@ class MainActivity : ComponentActivity() {
         MaterialTheme() {
 
             Column(
-                modifier = Modifier.padding(16.dp).fillMaxHeight(),
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxHeight(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.jetlogo),
-                    contentDescription = "Jetpack Compose logo",
-                    modifier = Modifier
-                        .width(300.dp)
-                        .height(300.dp)
-                )
+                MyImage()
                 Spacer(modifier = Modifier.height(16.dp))
                 Text(text = "Mi nombre es $name", style = MaterialTheme.typography.h6, color = MaterialTheme.colors.error)
                 Text(text = "Estoy aprendiendo jetpack Compose", style = MaterialTheme.typography.h6)
                 Text(text = "Lo organizo en columna", style = MaterialTheme.typography.h6, color = MaterialTheme.colors.primaryVariant)
             }
         }
+    }
+    @Composable
+    private fun MyImage(){
+        Image(
+            painter = painterResource(id = R.drawable.jetlogo),
+            contentDescription = "Jetpack Compose logo",
+            modifier = Modifier
+                .width(300.dp)
+                .height(300.dp)
+        )
     }
 
     @Preview
